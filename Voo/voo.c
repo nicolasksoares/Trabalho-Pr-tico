ff346670b1005542b54b209d0191fbe862731ed7
Nicolas Kiffer <nicolask.soares2@gmail.com>
2024-12-04 20:39:07 -0300

    Origem, Destino e tarifa
    Adição da funcionalidade de adicionar origem, destino e tarifa do vôo.

--- Voo/voo.c
+++ Voo/voo.c
@@ -9,10 +9,11 @@ typedef struct {
     int id_Copiloto;
     int id_Comissario;
     char status[10];
-    int tarifa;
+    float tarifa;
     int hora;
     int data;
-    char nome[100];
+    char origem[100];
+    char destino[100];
 } VOO;
 
 int main() {
@@ -33,36 +34,55 @@ int main() {
         voo[i].id_Voo = i + 1;
     }
 
-    for (i=0;i<quantidade;i++) {
-        printf("\nRegistrando dados do voo %d \n",i+1);
+    // Registrando os dados de cada voo
+    for (i = 0; i < quantidade; i++) {
+        printf("\nRegistrando dados do voo %d \n", i + 1);
 
-    // Inserção do código do piloto
-        printf("Insira o id do piloto:\n", voo[i].id_Voo);
+        getchar();
+
+        // Inserção da origem do voo
+        printf("Insira a origem do voo: \n");
+        fgets(voo[i].origem, 100, stdin);
+        voo[i].origem[strcspn(voo[i].origem, "\n")] = '\0'; 
+
+        // Inserção do destino do voo
+        printf("Insira o destino do voo: \n");
+        fgets(voo[i].destino, 100, stdin);
+        voo[i].destino[strcspn(voo[i].destino, "\n")] = '\0'; 
+
+        // Inserção do código do piloto
+        printf("Insira o id do piloto para o voo id = %d: \n", voo[i].id_Voo);
         scanf("%d", &voo[i].id_Piloto);
 
-    // Inserção do código do copiloto
-        printf("Insira o id do Copiloto:\n", voo[i].id_Voo);
+        // Inserção do código do copiloto
+        printf("Insira o id do Copiloto para o voo id = %d: \n", voo[i].id_Voo);
         scanf("%d", &voo[i].id_Copiloto);
 
-    // Inserir do código do comissário
-        printf("Insira o id do Comissario:\n", voo[i].id_Voo);
-        scanf("%d", &voo[i].id_Comissario);      
+        // Inserção do código do comissário
+        printf("Insira o id do Comissario para o voo id = %d: \n", voo[i].id_Voo);
+        scanf("%d", &voo[i].id_Comissario);
 
-        printf("Insira se o voo id = %d esta ativo ou inativo \n", voo[i].id_Voo);
-        scanf("%s",voo[i].status);  
+        // Inserção do status do voo
+        printf("Insira se o voo id = %d esta ativo ou inativo: \n", voo[i].id_Voo);
+        scanf("%s", voo[i].status);
 
+        // Inserção da tarifa do voo
+        printf("Insira a tarifa do voo id = %d: \n", voo[i].id_Voo);
+        scanf("%f", &voo[i].tarifa);
     }
-    printf("\n");
 
     // Exibindo os dados de cada voo
-    for (i=0; i<quantidade;i++) {
+    for (i = 0; i < quantidade; i++) {
         printf("\nVoo %d: id = %d\n"
+               "Origem: %s\n"
+               "Destino: %s\n"
                "Piloto: id = %d\n"
                "Copiloto: id = %d\n"
                "Comissario: id = %d\n"
                "Status do voo: %s\n"
-               "Tarifa:\n", 
-               i + 1, voo[i].id_Voo, voo[i].id_Piloto, voo[i].id_Copiloto, voo[i].id_Comissario,
+               "Tarifa: %.2f\n", 
+               i + 1, voo[i].id_Voo, voo[i].origem, voo[i].destino, 
+               voo[i].id_Piloto, voo[i].id_Copiloto, voo[i].id_Comissario,
                voo[i].status, voo[i].tarifa);
     }
 
