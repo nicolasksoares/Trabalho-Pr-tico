fa4011cc7aeab6c717ef4365adebea6b41a98f73
Nicolas Kiffer <nicolask.soares2@gmail.com>
2024-12-04 15:46:03 -0300

    Criação da funcionalidade de cadastrar voo
    Criação da estrutura de cadastro de voo;

---
+++ Vôo/voo.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int id_Voo;
+    int id_Piloto;
+    int id_Copiloto;
+    int id_Comissario;
+    int status;
+    int tarifa;
+    int hora;
+    int data;
+    char nome[100];
+} VOO;
+
+int main(){
+
+}
