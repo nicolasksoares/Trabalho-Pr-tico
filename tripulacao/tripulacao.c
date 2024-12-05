5d368abf67ddd150b3dc0f365cae9cba176e1027
Nicolas Kiffer <nicolask.soares2@gmail.com>
2024-12-04 21:05:04 -0300

    Correção do id do tripulante
    Correção do id do tripulante que estava começando errado.

--- tripulacao/tripulacao.c
+++ tripulacao/tripulacao.c
@@ -11,25 +11,29 @@ typedef struct {
     char telefone[14];
 } Tripulacao;
 
-void registrarTripulacao(Tripulacao tripulacao[],int *quantidadeTripulacao){
-    
+void registrarTripulacao(Tripulacao tripulacao[], int *quantidadeTripulacao) {
     if (*quantidadeTripulacao < MAX_TRIPULACAO) {
         Tripulacao novaTripulacao;
 
-        novaTripulacao.id = *quantidadeTripulacao + 1;   
+        novaTripulacao.id = *quantidadeTripulacao + 1;
 
-        printf("Digite o nome da tripulacao: ");
-        scanf("%s", novaTripulacao.nome);
-        
-        printf("Digite o cargo da tripulacao: ");
-        scanf("%s", novaTripulacao.cargo);
-        
-        printf("Digite o telefone da tripulacao: ");
-        scanf("%s", novaTripulacao.telefone);
-        
+        getchar();
+
+        printf("Digite o nome do tripulante: ");
+        fgets(novaTripulacao.nome, sizeof(novaTripulacao.nome), stdin);
+        novaTripulacao.nome[strcspn(novaTripulacao.nome, "\n")] = '\0';
+
+        printf("Digite o cargo do tripulante: ");
+        fgets(novaTripulacao.cargo, sizeof(novaTripulacao.cargo), stdin);
+        novaTripulacao.cargo[strcspn(novaTripulacao.cargo, "\n")] = '\0';
+
+        printf("Digite o telefone do tripulante: ");
+        fgets(novaTripulacao.telefone, sizeof(novaTripulacao.telefone), stdin);
+        novaTripulacao.telefone[strcspn(novaTripulacao.telefone, "\n")] = '\0';
+
         tripulacao[*quantidadeTripulacao] = novaTripulacao;
         (*quantidadeTripulacao)++;
-        
+
         printf("Tripulacao registrada com sucesso!\n");
     } else {
         printf("Nao ha espaco suficiente para mais tripulantes.\n");
@@ -41,7 +45,7 @@ void mostrarTripulacao(Tripulacao tripulacao[], int quantidadeTripulacao) {
         printf("Nao ha tripulantes registrados.\n");
         return;
     }
-    
+
     printf("Lista de Tripulantes:\n");
     for (int i = 0; i < quantidadeTripulacao; i++) {
         printf("ID: %d\n", tripulacao[i].id);
@@ -52,41 +56,39 @@ void mostrarTripulacao(Tripulacao tripulacao[], int quantidadeTripulacao) {
     }
 }
 
-int main(){
-    Tripulacao tripulacao[MAX_TRIPULACAO];    
-    int quantidadeTripulacao=0;
+int main() {
+    Tripulacao tripulacao[MAX_TRIPULACAO];
+    int quantidadeTripulacao = 0; 
     int opcao;
-    int i=0;
 
-    printf("Informe a quantidade de tripulantes: \n");
-    scanf("%d",&quantidadeTripulacao);
-    
-for (i = 0; i < quantidadeTripulacao; i++) {
-    printf("Escolha uma opcao:\n"
-           "[1] Cadastrar Tripulacao\n"
-           "[2] Mostrar Tripulacao\n"
-           "[3] Sair\n");
-    scanf("%d", &opcao);
-
-    while (opcao != 1 && opcao != 2 && opcao != 3) {
-        printf("Opcao invalida! Tente novamente.\n");
+    // Menu principal
+    while (1) {
+        printf("\nEscolha uma opcao:\n"
+               "[1] Cadastrar Tripulacao\n"
+               "[2] Mostrar Tripulacao\n"
+               "[3] Sair\n");
         scanf("%d", &opcao);
-    }
 
-    switch (opcao) {
-        case 1:
-            registrarTripulacao(tripulacao, &quantidadeTripulacao);
-            break;
-        case 2:
-            mostrarTripulacao(tripulacao, quantidadeTripulacao);
-            break;
-        case 3:
-            printf("Saindo do programa.\n");
-            return 0;
-        default:
-            break;
+        // Verificar se a opção é válida
+        while (opcao != 1 && opcao != 2 && opcao != 3) {
+            printf("Opcao invalida! Tente novamente.\n");
+            scanf("%d", &opcao);
+        }
+
+        switch (opcao) {
+            case 1:
+                registrarTripulacao(tripulacao, &quantidadeTripulacao);
+                break;
+            case 2:
+                mostrarTripulacao(tripulacao, quantidadeTripulacao);
+                break;
+            case 3:
+                printf("Saindo do programa.\n");
+                return 0;
+            default:
+                break;
+        }
     }
-}
 
     return 0;
 }
